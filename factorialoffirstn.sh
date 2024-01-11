d16ebe1f1c3a6b21ffc482b7841836cc9f73aa0b
gshashi1408 <154942929+gshashi1408@users.noreply.github.com>
2024-01-12 02:10:18 +0530

    Update factorialoffirstn.sh

--- factorialoffirstn.sh
+++ factorialoffirstn.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "enter the value of the number"
-num=$1
+read $1
 result=1
-while [ $num -gt 1 ]
+while [ $1 -gt 1 ]
 do
-result=`expr $num \* $result`
-num=`$num - 1`
+result=`expr $1 \* $result`
+1=`$1 - 1`
 done
 echo "factorial of first n numbers is $result"
